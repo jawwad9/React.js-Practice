7be7752c8829679101b9b4d9f19d525d14abc372
jawwad9 <jawwadmuhammal514@gmail.com>
2024-09-16 23:52:21 +0500

    Complete

---
+++ Number-Counter/src/App.jsx
@@ -0,0 +1,63 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  let [num, setNum] = useState(0)
+  function addNum() {
+    setNum(num + 1)
+
+
+    // agar aap 1 dafama 2 bar setNum ka function run karna chata ha to nicha diagay tarika sa nahi hoga
+    setNum(num + 1)
+    setNum(num + 1)
+
+
+    // agar aap 1 dafama 2 bar setNum ka function run karna chata ha to nicha diagay tarika sa hojayga but bad practice ha.
+    setNum(num += 1)
+    setNum(num += 1)
+
+
+    // agar aap 1 dafama 2 bar setNum ka function run karna chata ha to nicha diagay tarika sa hojayga or ya good practice ha.
+    setNum(prevVal => prevVal + 1);
+    setNum(prevVal => prevVal + 1);
+
+  }
+  function lessNum() {
+    if (num === 0) {
+      alert(`The value does not go below zero`)
+    } else {
+      setNum(num - 1)
+    }
+  }
+  function resetNum() {
+    if (num === 0) {
+      alert(`number is already reset`)
+    } else {
+      setNum(num = 0)
+    }
+  }
+  console.log(num);
+
+  return (
+    <>
+      <div className="container mt-5 p-5 " >
+        <div>
+        <h1 className="text-center m-5">{num}</h1>
+        </div>
+        <div className="button-group gap-3 d-flex justify-content-center align-self-center flex-wrap">
+          <div>
+          <button className="btn btn-outline-primary" onClick={addNum}>Add</button>
+          </div>
+          <div>
+          <button className="btn btn-outline-primary" onClick={lessNum}>Less</button>
+          </div>
+          <div>
+          <button className="btn btn-outline-danger" onClick={resetNum}>Reset</button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
