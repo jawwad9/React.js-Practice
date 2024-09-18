a954556d3a756a15112ec3c6d857f30e44806fb0
jawwad9 <jawwadmuhammal514@gmail.com>
2024-09-18 14:33:11 +0500

    commit

---
+++ 00 Class Practice/1 class/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react"
+
+
+function App(){
+  const [num, setNum] = useState(0);
+  const addNum = ()=>{
+    setNum(num + 1);
+    console.log(num)
+  }
+
+const lessNum = ()=>{
+setNum(num - 1);
+console.log(num)
+}
+  return(
+    <>
+        <h1>Hello World</h1><br />
+        <h1>Add {num}</h1><br />
+        <p>{num}</p>
+        <button onClick={addNum}>Add {num}</button>
+        <button onClick={lessNum}>Less {num}</button>
+    </>
+  )
+}
+
+export default App
+
