3afe83c318c89a2f160d384b8ae4310bc8231f0e
jawwad9 <jawwadmuhammad514@gmail.com>
2024-09-20 17:29:09 +0500

    complete

--- 00 Class Practice/1 class/src/App.jsx
+++ 00 Class Practice/1 class/src/App.jsx
@@ -15,10 +15,13 @@ console.log(num)
   return(
     <>
         <h1>Hello World</h1><br />
+        <h1>UseState</h1>
+
         <h1>Add {num}</h1><br />
         <p>{num}</p>
         <button onClick={addNum}>Add {num}</button>
         <button onClick={lessNum}>Less {num}</button>
+
     </>
   )
 }
