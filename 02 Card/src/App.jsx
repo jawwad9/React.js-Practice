c7e5439fbeca432eff2cbc88f7ff25823e6abcbf
jawwad9 <jawwadmuhammad514@gmail.com>
2024-09-21 19:52:08 +0500

    Complete

--- 02 Card/src/App.jsx
+++ 02 Card/src/App.jsx
@@ -6,8 +6,8 @@ export const App = () => {
 <>
 <h1>Hello World</h1>
 <Card src = "https://media.wisemarket.com.pk/variant/SamsungGalaxyA555GBrandNewAwesomeLemon256GB8GBRAM-15.webp" price = "130999" title = "Samsung Galaxy A55 5G" />
-<Card src = "" price = "" title = "" />
-<Card src = "" price = "" title = "" />
+
+
 
 </>
   )
--- 02 Card/src/components/Card.jsx
+++ 02 Card/src/components/Card.jsx
@@ -1,30 +1,27 @@
-// import React from 'react'
+import React from 'react'
 
-// const Card = () => {
-//   return (
-// <>
-// <h1>Card Title</h1>
-// <p>Lorem ipsum dolor sit amet consectetur adipisicing
-//      elit. Nesciunt, asperiores?</p>
-// <p>Rs; 2000</p>
-
-// </>  )
-// }
-
-
-// export default Card
-
-
-import React, { useState } from 'react'
-
-const Card = (props) => {
+const Card = ({src, price, title}) => {
   return (
 <>
-<img src={props.src} alt="" />
-<h1>PKR: {props.price}</h1>
-<h2>{props.title}</h2>
+<img src={src} alt="" />
+<h1>PKR:{price}</h1>
+<h2>{title}</h2>
 </>  
 )
 }
-
 export default Card
+
+
+// import React, { useState } from 'react'
+
+// const Card = (props) => {
+//   return (
+// <>
+// <img src={props.src} alt="" />
+// <h1>PKR: {props.price}</h1>
+// <h2>{props.title}</h2>
+// </>  
+// )
+// }
+
+// export default Card
