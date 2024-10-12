9f9a8f5296e103f37cb65ef205239721822a0a77
jawwad9 <jawwadmuhammad514@gmail.com>
2024-10-12 22:00:23 +0500

    complete

--- Quiz-App/src/App.jsx
+++ Quiz-App/src/App.jsx
@@ -1,6 +1,5 @@
 import axios from "axios";
-import React, { useState, useRef } from "react";
-import { useEffect } from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 function App() {
   const [question, setQuestion] = useState([]);
@@ -8,7 +7,6 @@ function App() {
 
 
   // API handling
-
   const checkedInput = useRef([]);
   useEffect(() => {
     axios("https://the-trivia-api.com/v2/questions")
@@ -23,11 +21,9 @@ function App() {
 
 
   //  Shuffle array function
-
   function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
-
       [array[i], array[j]] = [array[j], array[i]];
     }
     
@@ -35,7 +31,6 @@ function App() {
   }
 
   // Update Question and selected answer
-
   function nextQuestion (index){
     const checkedButton = checkedInput.current.find(input => input.checked);
     if (checkedButton) {
@@ -53,16 +48,16 @@ function App() {
   return (
     <>
     <div className="mx-5 p-5">
-       <h1 className="text-center m-3">Quiz App</h1>
+       <h1 className="text-center mt-3">Quiz App</h1>
 
       <div className="">
-      {question.length > 0 ? <div className=" p-5 m-5 bg-gray-100 items-center justify-center ">
+      {question.length > 0 ? <div className=" p-5 mt-5 bg-gray-101 items-center justify-center ">
         <div>
         <h1>Q{questionState + 1}: {question[questionState].question.text}</h1>
         </div>
         <div className="mt-4  ">
           {shuffleArray([...question[questionState].incorrectAnswers , question[questionState].correctAnswer]).map((item , index)=>{
-            return <li key={index} className="mt-3 ">
+            return <li key={index} className="mt-4 ">
             <input type="radio" name='choice' id={item} value={item} ref={el => (checkedInput.current[index] = el)}/>
             <label htmlFor={item}>{item}</label>
           </li>
