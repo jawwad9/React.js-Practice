11b06301138c52537705a3e6357a6cff2ef3f748
jawwad9 <jawwadmuhammad514@gmail.com>
2024-10-17 16:40:58 +0500

    firebase firestore complete

--- firebase-app/src/pages/Home.jsx
+++ firebase-app/src/pages/Home.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { auth, db  } from '../config/firebase/firebaseconfig';
 import { useNavigate } from 'react-router-dom';
 import { onAuthStateChanged } from 'firebase/auth';
-import { collection, addDoc } from "firebase/firestore"; 
+import { collection, addDoc, getDocs, Timestamp  } from "firebase/firestore"; 
 
 
 const Home = () => {
@@ -10,6 +10,7 @@ const Home = () => {
     const navigate = useNavigate();
     const title = useRef();
     const dec = useRef();
+    const [storeData, setStoreData] = useState([]);
 
 
 // firebase  onAuthStateChanged
@@ -27,7 +28,7 @@ const Home = () => {
       }, []);
 
 
-//  firebade firestore
+//  firebade firestore and manual render data
 
     const todoTitle = async (event) => {
         event.preventDefault();
@@ -38,8 +39,17 @@ const Home = () => {
             const docRef = await addDoc(collection(db, "store"), {
               title: title.current.value,
               dec: dec.current.value,
-              uid: auth.currentUser.uid
-            });
+              uid: auth.currentUser.uid,
+              postDate: Timestamp.fromDate(new Date()),
+
+            })
+            storeData.push({
+                title: title.current.value,
+                dec: dec.current.value,
+                uid: auth.currentUser.uid
+              });        
+              setStoreData([...storeData]);
+              console.log(storeData);
             console.log("Document written with ID: ", docRef.id);
           } catch (e) {
             console.error("Error adding document: ", e);
@@ -49,57 +59,42 @@ const Home = () => {
 
 
 
-     
-      
+ // firebase render data  
+ 
+async function renderDate() {
+    const querySnapshot = await getDocs(collection(db, "store"));
+    querySnapshot.forEach((doc) => {
+      console.log(doc.data());
+      storeData.push(doc.data());
+    });
+     }
 
+
+
+     useEffect(() => {
+        renderDate(); // Call the async function on component mount
+        setStoreData(storeData);      
+
+    }, [])    
   return (
         <>
 
 
-
+        <h1>Home</h1>
         <form onSubmit={todoTitle}>
             <input type="text" placeholder='Enter your Text' ref={title}/>
             <input type="text" placeholder='Enter your Text' ref={dec}/>
             <button type='sumbit'>Click</button>
         </form>
-        {/* <div className="carousel w-full">
-  <div id="slide1" className="carousel-item relative w-full">
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp"
-      className="w-full" />
-    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-      <a href="#slide4" className="btn btn-circle">❮</a>
-      <a href="#slide2" className="btn btn-circle">❯</a>
-    </div>
-  </div>
-  <div id="slide2" className="carousel-item relative w-full">
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp"
-      className="w-full" />
-    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-      <a href="#slide1" className="btn btn-circle">❮</a>
-      <a href="#slide3" className="btn btn-circle">❯</a>
-    </div>
-  </div>
-  <div id="slide3" className="carousel-item relative w-full">
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp"
-      className="w-full" />
-    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-      <a href="#slide2" className="btn btn-circle">❮</a>
-      <a href="#slide4" className="btn btn-circle">❯</a>
-    </div>
-  </div>
-  <div id="slide4" className="carousel-item relative w-full">
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp"
-      className="w-full" />
-    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-      <a href="#slide3" className="btn btn-circle">❮</a>
-      <a href="#slide1" className="btn btn-circle">❯</a>
-    </div>
-  </div>
-</div> */}
+        {
+            storeData.map((item) => {
+                return <div key={item.uid}>
+                    <h1>{item.title}</h1>
+                    <h1>{item.dec}</h1>
+                    <h1>{item.uid}</h1>
+                </div>
+            })
+        }<br></br>
         </>
   )
 }
