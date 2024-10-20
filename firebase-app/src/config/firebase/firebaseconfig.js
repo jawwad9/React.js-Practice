20f0e6432bfb378043ec745748b0f94909906504
jawwad9 <jawwadmuhammad514@gmail.com>
2024-10-21 00:56:16 +0500

    complete

--- firebase-app/src/config/firebase/firebaseconfig.js
+++ firebase-app/src/config/firebase/firebaseconfig.js
@@ -1,6 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
 
 
 const firebaseConfig = {
@@ -16,4 +18,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app)
 export const db = getFirestore(app);
+export const storage = getStorage(app);
+
 
